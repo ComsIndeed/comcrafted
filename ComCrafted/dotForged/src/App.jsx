14ba4a267031eed18df40f8003e8baf9d54c44fa
ComsIndeed <TrulyComs2023@gmail.com>
2023-11-06 19:43:43 +0800

    Website changes

--- ComCrafted/dotForged/src/App.jsx
+++ ComCrafted/dotForged/src/App.jsx
@@ -1,47 +1,56 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-
   return (
     <>
+      <NavigationBar header="dotForge" />
 
-    <NavigationBar header="dotForge" />
-
-    <Card></Card>
-    
+      <CardContainer>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </CardContainer>
     </>
-  )
+  );
 }
 
 function NavigationBar(props) {
   return (
     <>
-    <div className='navBar'>
-      <h1> {props.header} </h1>
-    </div>
+      <div className="navBar">
+        <h1 id="header"> {props.header} </h1>
+      </div>
     </>
-  )
+  );
+}
+
+function CardContainer(props) {
+  return (
+    <>
+      <div className="card-container">{props.children}</div>
+    </>
+  );
 }
 
 function Card() {
   return (
     <>
-    <div className='card'>
-      <h3>Title</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis voluptas consequatur labore consequuntur sit fugiat ipsam rerum beatae odit iusto deserunt eaque officia molestias pariatur, quas corporis similique assumenda perspiciatis!</p>
-    </div>
+      <div className="card">
+        <div className="card-icon" />
+        <div className="card-info">
+          <h3 className="card-header">Title</h3>
+          <p className="card-overview">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis
+            voluptas consequatur labore consequuntur sit fugiat ipsam rerum
+            beatae odit iusto deserunt eaque officia molestias pariatur, quas
+            corporis similique assumenda perspiciatis!
+          </p>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-
-
-
-
-
-
-
-
-
-
-export default App
+export default App;
