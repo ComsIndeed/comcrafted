a597585f96f081bc6c874198da09ff0d418ee402
ComsIndeed <TrulyComs2023@gmail.com>
2023-11-06 16:48:34 +0800

    Switched to React

--- ComCrafted/src/App.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import Topbar from './components/Topbar.svelte';
-  import Contents from './components/Contents.svelte'
-
-  let currentScreen
-</script>
-
-<Topbar />
-<Contents />
--- ComCrafted/src/components/Topbar.svelte
+++
@@ -1,6 +0,0 @@
-<div class="topbar">
-  <div id="brand">
-    <div id="icon" />
-    <h1 id="name"> dotForge</h1>
-  </div>
-</div>
